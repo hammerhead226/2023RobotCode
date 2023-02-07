3f7e780d86850f024d18cf96b5eb079f93384578
Nihil Suthy <52764984+nihilsuthy@users.noreply.github.com>
2023-02-06 19:05:01 -0500

    added notes on what needs to be added to Elevator code
    we need to create separate commands for the setLow, setMid, and setHigh

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -10,7 +10,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.libs.wrappers.GenericEncoder;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 
@@ -21,6 +23,21 @@ public class Elevator extends SubsystemBase {
   private PIDController pid;
   private boolean isManual; 
   private double initialPos;
+  private GenericEncoder gen;
+  private AnalogInput input = new AnalogInput(0);
+  /**
+   * Edits to make to Elevator:
+   * add 3 commands for Low, Mid, and High
+    * this is so that the elevator will keep moving until we need it to stop
+    * with the code right now, the driver will have to continuously press the button to run the InstantCommand
+   * create a method that stops the elevator once it gets ~10 or so ticks within setpoint
+    * check the shooter class from last year to get an example
+   * Create a GenericEncoder object to use instead of getSelectedSensorPosition();
+    * using a Magnum encoder
+    * the Falcon motor needs ~5.5 rotations to rotate elevator from top to bottom
+    * we are planning to create a 1:6 gear mechanism so that <1 rotation of Talon moves the elevator
+    * use GenericEncoder to see how many ticks rotated
+   */
 
   public Elevator() {
     elevatorTalon1 = new TalonFX(RobotMap.ELEVATOR_MOTOR_1);
@@ -37,6 +54,7 @@ public class Elevator extends SubsystemBase {
 
     //For Testing
     initialPos = elevatorTalon1.getSelectedSensorPosition(); 
+    gen = new GenericEncoder(input, 0, 2048, 0);
   }
 
   public void toggleManual(){
@@ -55,19 +73,19 @@ public class Elevator extends SubsystemBase {
   public void setLow()
   {
     elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.LOW_SETPOINT));
-    elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.LOW_SETPOINT));
+    elevatorTalon2.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.LOW_SETPOINT));
   }
 
   public void setMid()
   {
     elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.MID_SETPOINT));
-    elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.MID_SETPOINT));
+    elevatorTalon2.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.MID_SETPOINT));
   }
 
   public void setHigh()
   {
     elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.HIGH_SETPOINT));
-    elevatorTalon1.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.HIGH_SETPOINT));
+    elevatorTalon2.set(ControlMode.PercentOutput, pid.calculate(elevatorTalon1.getSelectedSensorPosition(), Constants.HIGH_SETPOINT));
   }
 
   //Potentially make one function and use lambdas to make simpler
