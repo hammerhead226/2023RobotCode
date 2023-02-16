41147b67c7569066934c13f500a0d26ce8c56ff0
SiddhantPatil24 <73323964+SiddhantPatil24@users.noreply.github.com>
2023-02-16 12:31:09 -0500

    balls
    small formatting change

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -13,7 +13,7 @@ public final class Autos {
   public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
-
+  
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");
   }
