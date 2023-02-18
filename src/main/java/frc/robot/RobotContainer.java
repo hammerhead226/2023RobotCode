234f10449fcac0a0e69eb449ded950027cfde3c4
David Chen <dychen2523@gmail.com>
2023-02-17 19:39:04 -0500

    removed comments lol

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,8 @@
 
 package frc.robot;
 
-import frc.libs.wrappers.Controller;
 import frc.robot.subsystems.LinearSlide;
-import edu.wpi.first.wpilibj.Joystick;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -25,9 +24,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final LinearSlide linearSlide = new LinearSlide();
-  // public Controller controller = new Controller(0,
-  // Constants.CONTROLLER_DEADBAND);
-  // Replace with CommandPS4Controller or CommandJoystick if needed
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
