3a039c58e881f97cc5176fe807eb9f527bd163bb
Joshua A <jasirvatham2602@gmail.com>
2023-03-01 19:52:16 -0500

    added methods to line up the limelight

--- src/main/java/frc/libs/wrappers/LimeLight.java
+++ src/main/java/frc/libs/wrappers/LimeLight.java
@@ -6,10 +6,12 @@ package frc.libs.wrappers;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTableEntry;
 
-/** 
+/**
  * @author Anish Chandra
- * Simplifies Data Collection from LimeLight
+ *         Simplifies Data Collection from LimeLight
  */
 public class LimeLight {
     private static NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
@@ -17,4 +19,18 @@ public class LimeLight {
     public static double getHorizontalOffset() {
         return limelight.getEntry("tx").getDouble(0.0);
     }
+
+    public static double getVerticalOffset() {
+        return limelight.getEntry("ty").getDouble(0.0);
+    }
+
+    public static double getTargetArea() {
+        return limelight.getEntry("ta").getDouble(0.0);
+    }
+
+    public static int isValidTarget() {
+        long x = 0;
+        return (int) (limelight.getEntry("tv").getInteger(x));
+    }
+
 }
